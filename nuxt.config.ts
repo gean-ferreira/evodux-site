4b449770807f30fab75f2c1cfad535df50bc517c
Gean Ferreira <gean_roberto@hotmail.com>
2023-08-17 15:57:09 -0300

    style: Add variáveis, grid e fonts da aplicação

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,15 @@ import { defineNuxtConfig } from 'nuxt/config'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "scss/main.scss";',
+        },
+      },
+    },
+  },
   app: {
     head: {
       link: [
