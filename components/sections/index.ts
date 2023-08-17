c42b0c870744412a9e3cc46d0b79134c5ebb98e5
Gean Ferreira <gean_roberto@hotmail.com>
2023-08-17 18:39:11 -0300

    feat: Add o layout default da aplicação

---
+++ components/sections/index.ts
@@ -0,0 +1,2 @@
+export { default as Aside } from './Aside.vue'
+export { default as Footer } from './Footer.vue'
---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="app container">
+    <Aside />
+    <section class="content">
+      <slot />
+    </section>
+  </section>
+  <Footer />
+</template>
+
+<script setup lang="ts">
+  import { Aside, Footer } from 'sections'
+</script>
+
+<style lang="scss" scoped>
+  .app {
+    @media (min-width: map-get($breakpoints, lg)) {
+      display: flex;
+    }
+
+
+    .content {
+      min-height: 100vh;
+    }
+  }
+</style>
