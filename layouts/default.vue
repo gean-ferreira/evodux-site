a0ab8bad44cb867dc182b376f5a3cebbe72b0148
Gean Ferreira <gean_roberto@hotmail.com>
2023-08-21 18:18:06 -0300

    feat: Add a funcionalidade de fechar e abrir no Aside no layout principal + responsividade

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,11 @@
 <template>
-  <section class="app container">
+  <section
+    class="app container"
+    :class="{ 'app--aside-open': asideStore.isOpen }"
+  >
     <Aside />
-    <section class="content">
+    <Hamburger />
+    <section @click="asideStore.close" class="content">
       <slot />
     </section>
   </section>
@@ -10,17 +14,59 @@
 
 <script setup lang="ts">
   import { Aside, Footer } from 'sections'
+  import Hamburger from 'elements/Hamburger'
+  import { useAsideStore } from 'stores/aside'
+  const asideStore = useAsideStore()
 </script>
 
 <style lang="scss" scoped>
   .app {
+    height: 100vh;
+    display: flex;
+
     @media (min-width: map-get($breakpoints, lg)) {
       display: flex;
     }
-
-
+    &.app--aside-open {
+      overflow: hidden;
+      @media (min-width: map-get($breakpoints, lg)) {
+      }
+    }
+    &.app--aside-open .content {
+      opacity: 0.3;
+      transform: translateX(60%);
+      overflow: hidden;
+      @media (min-width: map-get($breakpoints, lg)) {
+        transform: translateX(0);
+        overflow: auto;
+        opacity: 1;
+      }
+    }
     .content {
-      min-height: 100vh;
+      padding: 96px 24px 64px 24px;
+      overflow: auto;
+      transition: transform 0.3s ease;
+      flex: 2;
+      scrollbar-color: $secondary transparent;
+
+      &::-webkit-scrollbar {
+        width: 5px;
+      }
+
+      &::-webkit-scrollbar-track {
+        background: transparent;
+        border-radius: 10px;
+      }
+
+      &::-webkit-scrollbar-thumb {
+        height: 200px;
+        border-radius: 10px;
+        background: linear-gradient(transparent, $secondary, transparent);
+      }
+
+      @media (min-width: map-get($breakpoints, lg)) {
+        padding: 64px;
+      }
     }
   }
 </style>
