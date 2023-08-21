e0d9ed5402b58f24dc8a1b7ba9b2a31dacb4c36e
Gean Ferreira <gean_roberto@hotmail.com>
2023-08-21 17:53:23 -0300

    chore: Add Pinia ao projeto

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import { defineNuxtConfig } from 'nuxt/config'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ['@pinia/nuxt'],
   vite: {
     css: {
       preprocessorOptions: {
